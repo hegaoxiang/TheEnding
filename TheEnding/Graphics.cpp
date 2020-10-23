2f76e7044076de29f9b05fb68c28912a97927a30
12976 <12976@LAPTOP-8QGV5MI9>
2020-10-23 22:22:22 +0800

    just set False to Depth Test

--- TheEnding/Graphics.cpp
+++ TheEnding/Graphics.cpp
@@ -55,7 +55,7 @@ Graphics::Graphics(HWND hWnd)
 
 	// create depth stensil state
 	D3D11_DEPTH_STENCIL_DESC dsDesc = {};
-	dsDesc.DepthEnable = TRUE;
+	dsDesc.DepthEnable = FALSE;
 	dsDesc.DepthWriteMask = D3D11_DEPTH_WRITE_MASK_ALL;
 	dsDesc.DepthFunc = D3D11_COMPARISON_LESS;
 	ComPtr<ID3D11DepthStencilState> pDSState;
